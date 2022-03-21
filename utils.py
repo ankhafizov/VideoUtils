f1fd6d6a74214f0a8e80e5b99eb297497f0dc9d2
Anvar <ankhafizov1998@yandex.ru>
2022-03-21 20:31:48 +0300

    add empty

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import cv2
+from tqdm import tqdm
 
 
 def downsize(scale, input_video_path, output_video_filename, fps):
@@ -134,7 +135,47 @@ def h_merge(input_video_paths, output_video_filename, fps):
     cv2.destroyAllWindows()
 
 
+def add_empty_frames(where, secs, input_video_path, output_video_filename):
+    """
+    where - "start" or "end"
+    """
+
+    video_stream = cv2.VideoCapture(input_video_path)
+    fps = video_stream.get(cv2.CAP_PROP_FPS)
+    frame_width, frame_height = int(video_stream.get(3)), int(video_stream.get(4))
+    generate_black_frame = lambda: np.zeros((frame_height, frame_width, 3), dtype=np.uint8)
+
+    frames = []
+    while video_stream.isOpened():
+        ret, frame = video_stream.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        frames.append(frame)
+    count_frames_to_add = int(fps * secs)
+    out = cv2.VideoWriter(output_video_filename,
+                          cv2.VideoWriter_fourcc(*'XVID'),
+                          fps, (frame_width, frame_height))
+
+    if where == "start":
+        [out.write(generate_black_frame()) for _ in tqdm(range(count_frames_to_add))]
+        [out.write(f) for f in tqdm(frames)]
+    elif where == "end":
+        [out.write(f) for f in tqdm(frames)]
+        [out.write(generate_black_frame()) for _ in tqdm(range(count_frames_to_add))]
+    
+    video_stream.release()
+    out.release()
+
+
 if __name__ == "__main__":
-    fps = 25
-    reverse("plate-recognition-server_1.avi",
-            "plate-recognition-server.avi", fps)
+    # fps = 25
+    # reverse("plate-recognition-server_1.avi",
+    #         "plate-recognition-server.avi", fps)
+    
+    #flip(1, "plate-recognition-server-reverse1.avi", "plate-recognition-server-reverse2.avi", fps)
+
+    # h_merge(["plate-recognition-server_1.avi", "plate-recognition-server_2.avi"],
+    #          "IN_plate-recognition-server.avi", fps)
+
+    add_empty_frames("start", 10, "OUT_plate-recognition-server.avi", "OUT_plate-recognition-server1.avi")
